1a36bd584b1533ef5490bebe856cc2846d16d51e
Maris Dautov <maris.dautov28@gmail.com>
2025-05-28 14:33:52 +0500

    update function calculate_tax + test update

--- src/taxes.py
+++ src/taxes.py
@@ -15,7 +15,16 @@ def calculate_taxes(prices: list[float], tax_rate: float) -> list[float]:
     return taxed_prices
 
 
-def calculate_tax(price: float, tax_rate: float) ->float:
+def calculate_tax(price: float,
+                  tax_rate: float,
+                  *,
+                  discount: float = 0,
+                  round_digits: int = 2
+                  ) ->float:
+
+    for arg in [price,tax_rate, discount, round_digits]:
+        if not isinstance(arg, (int | float)):
+            raise TypeError("Ошибка типа данных")
 
     if price < 0:
         raise ValueError('Неверная цена')
@@ -23,5 +32,6 @@ def calculate_tax(price: float, tax_rate: float) ->float:
     if tax_rate < 0 or tax_rate >= 100:
         raise ValueError('Неверный налоговый процент')
 
-    result = price * tax_rate/100
-    return price + result
+    new_price = price + (price * tax_rate/100)
+    price_with_discount = new_price - (new_price * discount/100)
+    return round(price_with_discount, round_digits)
--- tests/test_taxes.py
+++ tests/test_taxes.py
@@ -39,3 +39,36 @@ def test_calculate_tax_invalid_tax_rate_bellow_zero():
 def test_calculate_tax_invalid_tax_rate_affter_100():
     with pytest.raises(ValueError):
         calculate_tax(100, 100)
+
+
+@pytest.mark.parametrize("price, tax_rate, discount, expected", [(100, 10, 0, 110.0),
+                                                                 (100, 10, 10, 99.0),
+                                                                 (100, 10, 100, 0.0)])
+def test_calculate_tax_with_discount(price, tax_rate, discount, expected):
+    assert calculate_tax(price, tax_rate, discount = discount) == expected
+
+def test_calculate_tax_with_no_discount():
+    assert calculate_tax(100, 10) == 110
+
+
+@pytest.mark.parametrize("round_digits, expected", [(0, 99),
+                                                    (1, 99.4),
+                                                    (2, 99.42),
+                                                    (3, 99.425)])
+
+def test_calculate_tax_round( round_digits, expected):
+    assert calculate_tax(100, 2.5,discount = 3, round_digits = round_digits ) == expected
+
+
+@pytest.mark.parametrize("price, tax_rate, discount, round_digits", [('100', 10, 0, 1),
+                                                                    (100, '10', 10, 1),
+                                                                    (100, 10, '100', 1),
+                                                                    (100, 10, 100, '1')])
+def test_calculate_tax_wrong_type(price, tax_rate, discount, round_digits):
+    with pytest.raises(TypeError):
+        calculate_tax(price, tax_rate, discount = discount, round_digits = round_digits)
+
+
+def test_calculate_tax_kwargs():
+    with pytest.raises(TypeError):
+        calculate_tax(100, 2, 3, 1)
